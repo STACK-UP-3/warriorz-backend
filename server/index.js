d4bd5aa486cb48d4b409d7a23ca82d8099c7ba12
Robin Hood <fordarnold@gmail.com>
2020-04-09 10:51:02 +0200

    bug(server): fix the Heroku PORT error (#18)

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.HTTP_PORT || 5000;
+const PORT = process.env.PORT || process.env.HTTP_PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
