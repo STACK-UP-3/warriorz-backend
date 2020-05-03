d15608ab96c0b29884e0e9000e3955c67613b58f
Robin Hood <fordarnold@gmail.com>
2020-05-03 12:06:36 +0200

    test(routing): add test case for landing route

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -7,8 +7,26 @@ import newUser from './fixtures/signupData';
 chai.should();
 chai.use(chaiHttp);
 
-describe('Route test', () => {
-  it('Incorrect route', done => {
+describe('>>> Testing Starter Routes', () => {
+  it('should return welcome message', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.type).to.equal('application/json');
+        expect(res.body).to.have.property('status');
+        expect(res.body).to.have.property('message');
+
+        expect(res.body.status).to.equal(200);
+        expect(res.body.message).to.equal(
+          'Welcome to Barefoot Nomad (Warriorz) app.',
+        );
+        done();
+      });
+  });
+
+  it('Incorrect route', (done) => {
     chai
       .request(app)
       .post('/api/v1/users/signupuududud')
