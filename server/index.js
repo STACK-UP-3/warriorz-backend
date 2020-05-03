3a41d119ea478ce2ed3bf20cdb7c25e19d85138a
Robin Hood <fordarnold@gmail.com>
2020-05-03 13:11:15 +0200

    feature(chats): add web sockets listener on server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
+import sockets from 'socket.io';
 import app from './app';
 
 const PORT = process.env.PORT || process.env.HTTP_PORT || 5000;
 
-app.listen(PORT, () => {
+// Server init
+const server = app.listen(PORT, () => {
   console.log(`\n Express Server - listening on port: ${PORT} \n`);
+});
+
+// Sockets init ~ https://youtu.be/UwS3wJoi7fY?t=168
+const io = sockets(server);
+// Listen for socket connection
+io.on('connection', (socket) => {
+  console.log('Socket connection - successful...', socket.server);
+
+  // Listen for messages passed from socket
+  socket.on('chat', (data) => {
+    // Save the message to storage
+
+    // Emit (Pass) the message to all socket clients
+    io.sockets.emit('chat', data);
+  });
 });
