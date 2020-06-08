94c9d24a5b21106715e746861c2006d0a130d8b9
Tony Stark <fordarnold@gmail.com>
2020-06-08 19:19:40 +0200

    bug(auth): set JWT expiry time to one week
    As agreed by the team.
    
    [Fixes #173229204]

--- server/helpers/resetEncode.js
+++ server/helpers/resetEncode.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const encode = data => {
-  const token = jwt.sign(data, process.env.JWT_KEY, { expiresIn: '1h' });
+export const encode = (data) => {
+  const token = jwt.sign(data, process.env.JWT_KEY, { expiresIn: '7d' });
   return token;
 };
 
-export const decode = token => {
+export const decode = (token) => {
   const payload = jwt.verify(token, process.env.JWT_KEY);
   return payload;
 };
